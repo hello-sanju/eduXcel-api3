ab03d5b0ec4c5735572ba0e1fdc130cb631afdb2
hello-sanju <151989191+hello-sanju@users.noreply.github.com>
2024-04-11 11:51:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -183,6 +183,8 @@ passport.deserializeUser((id, done) => {
 const allowedOrigins = [
   'https://eduxcel.vercel.app',
   'http://localhost:5173',
+    'https://lic-neemuch-jitendra-patidar.vercel.app',
+
     'https://sanjay-patidar.vercel.app',
 
   // Add more domains if needed
